7ccee712bee4f215fc47bdf65351bb910cc95f08
Kshitej Jadhav <kshitej@vt.edu>
2018-10-18 00:40:03 -0400

    testing parsing

---
+++ src/argParser.py
@@ -0,0 +1,49 @@
+import argparse
+import optparse
+
+def client_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-proc', dest='proc_ip',
+                        help='Setting the processor IP')
+
+    parser.add_argument('-action', dest='action_param',
+                        help='Setting the action')
+
+    parser.add_argument('-book', dest='book_info',
+                        help='Setting the book info')
+
+    parser.add_argument('-count', dest='count_value',
+                        help='Set the Bought/Sell Values')
+
+    #parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+    return parser
+
+def processor_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-storage', dest='bt_addr',
+                        help='Setting the Bluetooth Mac Addr')
+
+    parser.add_argument('-p', dest='port_num',
+                        help='Setting the port number')
+
+    parser.add_argument('-z', dest='socket_size',
+                        help='Setting the socket size')
+
+    return parser
+
+def storage_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', dest='port_num',
+                        help='Setting the port number')
+
+    parser.add_argument('-b', dest='backlog',
+                        help='Setting the backlog size')
+
+    parser.add_argument('-z', dest='socket_size',
+                        help='Setting the socket size')
+
+    return parser
---
+++ src/checkAction.py
@@ -0,0 +1,13 @@
+import json
+
+def check(args):
+    #check if BUY/SELL
+    # temp1 = ('ADD','DELETE','COUNT')
+    # if args.action_param is in temp:
+    #     r=1
+    # el
+    if args.action_param == 'ADD':
+        d = json.loads(str(args.book_info))
+        NameFlag = True if 'Name' in d['Msg'][0]['BookInfo'] else False
+    return NameFlag
+

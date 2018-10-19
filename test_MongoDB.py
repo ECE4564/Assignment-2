4d7b94bded13c0b6d43cd70e6186190d9e9029e2
James D Toussaint <jdt13@vt.edu>
2018-10-19 18:23:36 -0400

    Test file for MongoDB Class/expected usage

---
+++ test_MongoDB.py
@@ -0,0 +1,40 @@
+# Unit tests for MongoDB class
+
+import MongoDB
+import unittest
+
+
+db1 = MongoDB.MongoDB()
+
+# clear the database to remove duplicates
+db1.clear_db()
+
+test_post1 = {"Author": "Stephen King",
+              "Name": "It",
+              "Stock": 4}
+
+db1.insert(test_post1)
+
+#print(db1.find({"Author": "Stephen King"}))
+
+db1.change_stock({"Name": "It"}, 6)
+
+#print(db1.find({"Name": "It"}))
+
+test_post2 = {"Author": "Herman Melville",
+              "Name": "Moby Dick",
+              "Stock": 3}
+
+db1.insert(test_post2)
+
+#print(db1.find({"Name": "Moby Dick"}))
+
+print("First list all")
+db1.list_all()
+
+db1.remove({"Name": "It"})
+
+print("Second list all")
+db1.list_all()
+
+print(db1.get_stock({"Name": "Moby Dick"}))

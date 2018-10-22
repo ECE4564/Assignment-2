ddc71286189213178f846218e18d085c7b55f20b
mpocta30 <michaelpocta@yahoo.com>
2018-10-21 20:59:13 -0400

    Finalize stuff

--- Storage.py
+++ Storage.py
@@ -19,53 +19,55 @@ def signal_handler(sig, frame):
     print("\nExitting program...")
     led.stop = True
     threadLED.join()
+    threadMongo.join()
     sys.exit(0)
 
 def MongoControl():
-    # Get mac address
-    mac = get_bluetooth_mac_addr()
-    print(strftime("[%H:%M:%S] ", gmtime()) + "Created socket at " + mac + " on port " + str(port)) 
-    server_sock.bind(("",port))
+    while(led.stop == False):
+        # Get mac address
+        mac = get_bluetooth_mac_addr()
+        print(strftime("[%H:%M:%S] ", gmtime()) + "Created socket at " + mac + " on port " + str(port)) 
+        server_sock.bind(("",port))
 
-    # List for clients
-    print(strftime("[%H:%M:%S] ", gmtime()) + "Listening for client connections")
-    server_sock.listen(backlog)
+        # List for clients
+        print(strftime("[%H:%M:%S] ", gmtime()) + "Listening for client connections")
+        server_sock.listen(backlog)
 
-    client_sock,address = server_sock.accept()
-    print(strftime("[%H:%M:%S] ", gmtime()) + " Accepted connection from " + str(address) + " on port " + str(port))
+        client_sock,address = server_sock.accept()
+        print(strftime("[%H:%M:%S] ", gmtime()) + " Accepted connection from " + str(address) + " on port " + str(port))
 
-    data = json.loads(client_sock.recv(s_size).decode("utf-8"))
-    print(strftime("[%H:%M:%S] ", gmtime()) + " Received Payload: " + str(data))
+        data = json.loads(client_sock.recv(s_size).decode("utf-8"))
+        print(strftime("[%H:%M:%S] ", gmtime()) + " Received Payload: " + str(data))
 
-    # when data is received, initialize MongoDB
-    db_result = ""
+        # when data is received, initialize MongoDB
+        db_result = ""
 
-    # update database based on received action
-    action = data["Action"]
-##    if action == "ADD":
-##        db_result = db.insert(data["Msg"]["Book Info"])
-##    elif action == "BUY":
-##        db.change_stock(data["Msg"]["Book Info"], data["Msg"]["Count"])
-##    elif action == "SELL":
-##        db.change_stock(data["Msg"]["Book Info"], -data["Msg"]["Count"])  # not sure if this will negate the value correctly
-##    elif action == "LIST":
-##        db_result = db.list_all()
-##    elif action == "DELETE":
-##        db.remove(data["Msg"]["Book Info"])
-##    elif action == "COUNT":
-##        book_list = db.list_all()
-##        in_stock = 0
-##        for book in book_list:
-##            if book["stock"] != 0:
-##                in_stock += 1
-##        db_result = "OK: " + str(in_stock) + " books in stock"
+        # update database based on received action
+        #action = data["Action"]
+    ##    if action == "ADD":
+    ##        db_result = db.insert(data["Msg"]["Book Info"])
+    ##    elif action == "BUY":
+    ##        db.change_stock(data["Msg"]["Book Info"], data["Msg"]["Count"])
+    ##    elif action == "SELL":
+    ##        db.change_stock(data["Msg"]["Book Info"], -data["Msg"]["Count"])  # not sure if this will negate the value correctly
+    ##    elif action == "LIST":
+    ##        db_result = db.list_all()
+    ##    elif action == "DELETE":
+    ##        db.remove(data["Msg"]["Book Info"])
+    ##    elif action == "COUNT":
+    ##        book_list = db.list_all()
+    ##        in_stock = 0
+    ##        for book in book_list:
+    ##            if book["stock"] != 0:
+    ##                in_stock += 1
+    ##        db_result = "OK: " + str(in_stock) + " books in stock"
 
-    # send result
-    # something like:
-    client_sock.send(json.dumps({'Name': 'Michael'}));
+        # send result
+        # something like:
+        client_sock.send(json.dumps({'Name': 'Michael'}));
 
-    client_sock.close()
-    server_sock.close()
+        client_sock.close()
+        server_sock.close()
 
 def LEDShow():
     while(led.stop == False):
@@ -99,5 +101,3 @@ threadMongo.daemon = True
 
 threadLED.start()
 threadMongo.start()
-
-threadMongo.join()

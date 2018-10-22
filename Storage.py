62f79286305a524a4476d93765a802b1e95a4971
mpocta30 <michaelpocta@yahoo.com>
2018-10-21 20:24:48 -0400

    Update threading for LED and storgae

--- Storage.py
+++ Storage.py
@@ -4,6 +4,7 @@ from src.argParser import storage_parser
 import bluetooth
 import threading
 import signal
+import json
 import sys
 import LED
 import MongoDB
@@ -20,10 +21,6 @@ def signal_handler(sig, frame):
     threadLED.join()
     sys.exit(0)
 
-def checkprogram():
-    print('Kill val: ', kill_program)
-    return kill_program
-
 def MongoControl():
     # Get mac address
     mac = get_bluetooth_mac_addr()
@@ -32,12 +29,12 @@ def MongoControl():
 
     # List for clients
     print(strftime("[%H:%M:%S] ", gmtime()) + "Listening for client connections")
-    server_sock.listen(1)
+    server_sock.listen(backlog)
 
     client_sock,address = server_sock.accept()
     print(strftime("[%H:%M:%S] ", gmtime()) + " Accepted connection from " + str(address) + " on port " + str(port))
 
-    data = client_sock.recv(1024).decode("utf-8")
+    data = json.loads(client_sock.recv(s_size).decode("utf-8"))
     print(strftime("[%H:%M:%S] ", gmtime()) + " Received Payload: " + str(data))
 
     # when data is received, initialize MongoDB
@@ -45,23 +42,23 @@ def MongoControl():
 
     # update database based on received action
     action = data["Action"]
-    if action == "ADD":
-        db_result = db.insert(data["Book Info"])
-    elif action == "BUY":
-        db.change_stock(data["Book Info"], data["Count"])
-    elif action == "SELL":
-        db.change_stock(data["Book Info"], -data["Count"])  # not sure if this will negate the value correctly
-    elif action == "LIST":
-        db_result = db.list_all()
-    elif action == "DELETE":
-        db.remove(data["Book Info"])
-    elif action == "COUNT":
-        book_list = db.list_all()
-        in_stock = 0
-        for book in book_list:
-            if book["stock"] != 0:
-                in_stock += 1
-        db_result = "OK: " + str(in_stock) + " books in stock"
+##    if action == "ADD":
+##        db_result = db.insert(data["Msg"]["Book Info"])
+##    elif action == "BUY":
+##        db.change_stock(data["Msg"]["Book Info"], data["Msg"]["Count"])
+##    elif action == "SELL":
+##        db.change_stock(data["Msg"]["Book Info"], -data["Msg"]["Count"])  # not sure if this will negate the value correctly
+##    elif action == "LIST":
+##        db_result = db.list_all()
+##    elif action == "DELETE":
+##        db.remove(data["Msg"]["Book Info"])
+##    elif action == "COUNT":
+##        book_list = db.list_all()
+##        in_stock = 0
+##        for book in book_list:
+##            if book["stock"] != 0:
+##                in_stock += 1
+##        db_result = "OK: " + str(in_stock) + " books in stock"
 
     # send result
     # something like:
@@ -76,10 +73,6 @@ def LEDShow():
        # led.size = len(db_result)
         led.size = 254
         led.showVarieties()
-        #if kill_program == 1:
-         #   print('Exit')
-          #  led.turnOff()
-           # break
 
 # Get command line arguments
 args = storage_parser().parse_args()
@@ -92,8 +85,10 @@ signal.signal(signal.SIGINT, signal_handler)
 
 db = MongoDB.MongoDB()
 led = LED.LED(0, False)
-port = 1
-kill_program = False
+
+backlog = int(args.backlog)
+port = int(args.port_num)
+s_size = int(args.socket_size)
 
 # Start the two threads
 threadLED = threading.Thread(target=LEDShow, args=())
